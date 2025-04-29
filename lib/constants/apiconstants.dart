a8147bc329cc9a90526593b39e1adac009ed6462
Marcus Baw <marcusbaw@gmail.com>
2025-04-29 17:12:39 +0100

    refactor of API constants to remove them from their own file and add them to the api_service.dart file

--- lib/constants/apiconstants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class ApiConstants {
-  static String rcpchCensusEngineBaseUrl = dotenv.env['RCPCH_CENSUS_ENGINE_BASE_URL'] ?? '';
-  static String rcpchCensusEngineApiKey = dotenv.env['RCPCH_CENSUS_ENGINE_API_KEY'] ?? '';
-  static String rcpchPostcodesBaseUrl = dotenv.env['RCPCH_POSTCODES_BASE_URL'] ?? '';
-  static String rcpchPostcodesApiKey = dotenv.env['RCPCH_POSTCODES_API_KEY'] ?? '';
-
-}
--- lib/network/api_service.dart
+++ lib/network/api_service.dart
@@ -1,44 +1,50 @@
+// Flutter
 import 'dart:convert';
 import 'dart:developer';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import '../constants/apiconstants.dart';
+
+// RCPCH Imports
 import './imdresponse.dart';
 import './postcodesResponse.dart';
+
+
+// API Constants
+String rcpchCensusEngineApiKey = dotenv.env['RCPCH_CENSUS_ENGINE_API_KEY']!;
+String rcpchCensusEngineBaseUrl = dotenv.env['RCPCH_CENSUS_ENGINE_BASE_URL']!;
+String rcpchPostcodesApiKey = dotenv.env['RCPCH_POSTCODES_API_KEY']!;
+String rcpchPostcodesBaseUrl = dotenv.env['RCPCH_POSTCODES_BASE_URL']!;
+
 
 class ApiService {
   Future<PostcodeData?> getPostcodeResponse(String postcode) async {
     try {
-      var url = Uri.parse("${ApiConstants.rcpchPostcodesBaseUrl}/postcodes/$postcode");
+      var url = Uri.parse("$rcpchPostcodesBaseUrl/postcodes/$postcode");
       var response = await http.get(
         url,
         headers: {
-          'Ocp-Apim-Subscription-Key': ApiConstants.rcpchPostcodesApiKey,
+          'Ocp-Apim-Subscription-Key': rcpchPostcodesApiKey,
         },
       );
 
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
         PostcodeData apiPostcodeData = PostcodeData.fromJson(data['result']);
-        log(apiPostcodeData.toString());
         return apiPostcodeData;
       }
     } catch (e) {
       log(e.toString());
-      // var codes = Codes(adminDistrict: 'adminDistrict', adminCounty: 'adminCounty', adminWard: 'adminWard', parish: 'parish', parliamentaryConstituency: 'parliamentaryConstituency', ccg: 'ccg', ccgId: 'ccgId', ced: 'ced', nuts: 'nuts', lsoa: 'lsoa', msoa: 'msoa', lau2: 'lau2')
-      // var errorPostcode = PostcodeData(postcode: 'error', quality: 0, eastings: 0, northings: 0, country: 'country', nhsHa: 'nhsHa', longitude: 0, latitude: 0, europeanElectoralRegion: 'europeanElectoralRegion', primaryCareTrust: 'primaryCareTrust', region: 'region', lsoa: 'lsoa', msoa: 'msoa', incode: 'incode', outcode: 'outcode', parliamentaryConstituency: 'parliamentaryConstituency', adminDistrict: 'adminDistrict', parish: 'parish', adminCounty: 'adminCounty', adminWard: 'adminWard', ced: 'ced', ccg: 'ccg', nuts: 'nuts', codes: codes);
-      // return PostcodesResponse(status: 404, postcodeData: errorPostcode)
     }
     return null;
   }
 
   Future<ImdResponse?> getIMDResponse(String postcode) async {
     try {
-      var url = Uri.parse("${ApiConstants.rcpchCensusEngineBaseUrl}?postcode=$postcode");
+      var url = Uri.parse("$rcpchCensusEngineBaseUrl?postcode=$postcode");
       var response = await http.get(
         url,
         headers: {
-          'Subscription-Key': ApiConstants.rcpchCensusEngineApiKey,
+          'Subscription-Key': rcpchCensusEngineApiKey,
         },
       );
       if (response.statusCode == 200) {
@@ -48,9 +54,6 @@ class ApiService {
       }
     } catch (e) {
       log(e.toString());
-      // var codes = Codes(adminDistrict: 'adminDistrict', adminCounty: 'adminCounty', adminWard: 'adminWard', parish: 'parish', parliamentaryConstituency: 'parliamentaryConstituency', ccg: 'ccg', ccgId: 'ccgId', ced: 'ced', nuts: 'nuts', lsoa: 'lsoa', msoa: 'msoa', lau2: 'lau2')
-      // var errorPostcode = PostcodeData(postcode: 'error', quality: 0, eastings: 0, northings: 0, country: 'country', nhsHa: 'nhsHa', longitude: 0, latitude: 0, europeanElectoralRegion: 'europeanElectoralRegion', primaryCareTrust: 'primaryCareTrust', region: 'region', lsoa: 'lsoa', msoa: 'msoa', incode: 'incode', outcode: 'outcode', parliamentaryConstituency: 'parliamentaryConstituency', adminDistrict: 'adminDistrict', parish: 'parish', adminCounty: 'adminCounty', adminWard: 'adminWard', ced: 'ced', ccg: 'ccg', nuts: 'nuts', codes: codes);
-      // return PostcodesResponse(status: 404, postcodeData: errorPostcode)
     }
     return null;
   }
